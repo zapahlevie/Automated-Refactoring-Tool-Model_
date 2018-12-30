645bf882653b79e661272511c8e55e4e4877ecec
zapahlevie <zapahlevie@gmail.com>
2018-12-30 12:32:08 +0700

    ClassModel

---
+++ src/main/java/com/finalproject/automated/refactoring/tool/model/ClassModel.java
@@ -0,0 +1,42 @@
+package com.finalproject.automated.refactoring.tool.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@ToString
+@EqualsAndHashCode
+public class ClassModel {
+
+    private String packageName;
+
+    @Builder.Default
+    private List<String> imports;
+
+    private String contentOuter;
+
+    @Builder.Default
+    private List<String> keywords = new ArrayList<>();
+
+    private String name;
+
+    private String extend;
+
+    private String implement;
+
+    @Builder.Default
+    private List<String> attributes;
+
+    @Builder.Default
+    private List<MethodModel> methodModels;
+
+    private String contentInner;
+
+    private Long loc;
+}
